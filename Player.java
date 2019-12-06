f91c7141e464222258a2f8eaf76c92eb10c64025
Kepler472 <57158606+Kepler472@users.noreply.github.com>
2019-12-06 16:27:41 +0000

    Add files via upload

--- Player.java
+++ Player.java
@@ -3,17 +3,20 @@ import javafx.scene.input.KeyCode;
 public class Player extends Creature{
 	
 	private int tokenCount;
-	private int brownKeyCount;
+	
+	private int yellowKeyCount;
 	private int whiteKeyCount;
 	private int blueKeyCount;
+	
 	private boolean fireBoots;
 	private boolean flippers;
+	
 	//Sprite(for alive and for dead)
 	
 	public Player(int[] position, String[][] map, String img) {
 		super(position, map, img);
 		tokenCount = 0;
-		brownKeyCount = 0;
+		yellowKeyCount = 0;
 		whiteKeyCount = 0;
 		blueKeyCount = 0;
 		fireBoots = false;
@@ -24,7 +27,7 @@ public class Player extends Creature{
 	public void setInventory(int[] inventory){
 		
 		tokenCount = inventory[0];
-		brownKeyCount = inventory[1];
+		yellowKeyCount = inventory[1];
 		whiteKeyCount = inventory[2];
 		blueKeyCount = inventory[3];
 		
@@ -46,7 +49,7 @@ public class Player extends Creature{
 		int[] inv = new int[6];
 		
 		inv[0] = tokenCount;
-		inv[1] = brownKeyCount;
+		inv[1] = yellowKeyCount;
 		inv[2] = whiteKeyCount;
 		inv[3] = blueKeyCount;
 		inv[4] = (fireBoots) ? 1 : 0;
@@ -67,7 +70,10 @@ public class Player extends Creature{
 			
 			case RIGHT:
 			case D:
-				x++;
+				if(getAdjancentTiles()[1] != "Wall") {
+					if(checkDoor(1, keyCode))
+					x++;
+				}
 				break;
 				
 			case LEFT:
@@ -90,7 +96,70 @@ public class Player extends Creature{
 		}
 	}
 	
+	public boolean checkDoor(int positionToCheck, KeyCode  keyCode) {
+		
+		char doorColour = getAdjancentTiles()[positionToCheck].charAt(1);
+		
+		boolean hasKey = false;
+		
+		if(getAdjancentTiles()[positionToCheck].charAt(0) == 'D') {
+			
+			switch(doorColour) {
+			
+				case 'Y':
+					if(hasYellowKey()) {
+						hasKey = true;
+					}
+					break;
+					
+				case 'W':
+					if(hasWhiteKey()) {
+						hasKey = true; 
+					}
+					break;
+					
+				case 'B':
+					if(hasBlueKey()) {
+						hasKey = true;
+					}
+					break;
+				
+				default:
+					break;
+			}
+		}
+
+		if(hasKey) {
+			removeDoor(keyCode);
+		}
+		
+		return true;
+	}
 	
+	public void removeDoor(KeyCode keyCode) {
+		
+		switch(keyCode) {
+		
+			case RIGHT:
+				map[x + 1][y] = "Floor";
+				break;
+				
+			case LEFT:
+				map[x-1][y] = "Floor";
+				break;
+				
+			case UP:
+				map[x][y-1] = "Floor";
+				break;
+				
+			case DOWN:
+				map[x][y+1] = "Floor";
+				break;
+				
+			default:
+				break;
+		}
+	}
 	
 	public boolean hasEnoughTokens(int amount) {
 		if (tokenCount >= amount) {
@@ -105,17 +174,17 @@ public class Player extends Creature{
 		tokenCount++;
 	}
 	
-	public boolean hasBrownKey() {
-		if (brownKeyCount > 0) {
-			brownKeyCount--;
+	public boolean hasYellowKey() {
+		if (yellowKeyCount > 0) {
+			yellowKeyCount--;
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public void addBrownKey() {
-		brownKeyCount++;
+	public void addYellowKey() {
+		yellowKeyCount++;
 	}
 
 	public boolean hasWhiteKey() {
